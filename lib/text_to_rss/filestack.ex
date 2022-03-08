c6f3976c200fb8953d252b560492016e4a2b147a
Jon Madden <notjonmadden@rocketinsights.com>
2022-03-08 06:25:22 -0500

    adds tesla logger, inits in fn

--- lib/text_to_rss/filestack.ex
+++ lib/text_to_rss/filestack.ex
@@ -1,17 +1,20 @@
 defmodule TextToRss.Filestack do
   alias Tesla
 
-  @client Tesla.client([
-    {Tesla.Middleware.BaseUrl, "https://www.filestackapi.com/api/"},
-    Tesla.Middleware.JSON,
-    {Tesla.Middleware.Query, [key: Application.get_env(:text_to_rss, :filestack_api_key)]}
-  ])
-
   def copy_file_from_uri(file_uri) do
     headers = [{"content-type", "application/x-www-form-urlencoded"}]
 
-    IO.puts("Uploading from #{file_uri}")
+    client() |> Tesla.post("store/S3", "url=#{file_uri}", headers: headers)
+  end
 
-    @client |> Tesla.post("store/S3", "url=#{file_uri}", headers: headers)
+  defp client do
+    api_key = Application.get_env(:text_to_rss, :filestack_api_key)
+
+    Tesla.client([
+      Tesla.Middleware.JSON,
+      {Tesla.Middleware.BaseUrl, "https://www.filestackapi.com/api/"},
+      {Tesla.Middleware.Query, key: api_key},
+      Tesla.Middleware.Logger
+    ])
   end
 end
