6fc3e9cbf0387928d6068c81df28300670a012e3
Jon Madden <notjonmadden@rocketinsights.com>
2022-03-17 09:29:06 -0400

    adds image messages to feed entry body

--- lib/text_to_rss_web/controllers/rss_controller.ex
+++ lib/text_to_rss_web/controllers/rss_controller.ex
@@ -5,8 +5,11 @@ defmodule TextToRssWeb.RssController do
 
   def rss_feed(conn, _params) do
     items = Feed.get_feed_items()
+    first = items |> Enum.at(0)
+    feed_updated_at = if is_nil(first), do: nil, else: first.inserted_at
 
     conn
+    |> assign(:feed_updated_at, feed_updated_at)
     |> assign(:items, items)
     |> render("rss_feed.xml")
   end
